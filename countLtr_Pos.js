db0f3424329d92abc7eb13c7df1c8a3866a9a909
Keawe <account@keawewong.com>
2017-04-05 12:03:40 -0400

    Finished the 'Find letter positions' exercise. Please run it in Node like the previous exercise.'

---
+++ countLtr_Pos.js
@@ -0,0 +1,16 @@
+function countletters(str) {
+
+    var obj = {};
+    var str = str.join('').toLowerCase();
+    var i = 0
+
+    for (char of str) {
+        obj[char] = (!obj[char] ? [i] : obj[char].concat(i));
+        i++
+    }
+
+    return obj
+
+}
+
+console.log(countletters(process.argv.slice(2)))
