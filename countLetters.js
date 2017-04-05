390090a556085975f5c691a03111eee2174e8dc0
Keawe <account@keawewong.com>
2017-04-04 23:02:14 -0400

    Completed countLetters.js. Please run it using Command line in Node. Ex: Node countLetters.js your string.

---
+++ countLetters.js
@@ -0,0 +1,14 @@
+function countletters(str) {
+
+    var obj = {};
+    var str = str.join('').toLowerCase();
+
+    for (chr of str) {
+        obj[chr] = (isNaN(obj[chr]) ? 1 : ++obj[chr]);
+    }
+
+    return obj
+
+}
+
+console.log(countletters(process.argv.slice(2)))
